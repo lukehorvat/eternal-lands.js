952ae81266a1b7affa1651361718dd5f868bdc31
Luke Horvat <lukehorvat@gmail.com>
2022-04-12 20:39:56 +0200

    Create examples

---
+++ examples/connect.ts
@@ -0,0 +1,35 @@
+import { Connection, ClientPacketType, ServerPacketType } from '../lib';
+
+(async () => {
+  const connection = new Connection();
+
+  try {
+    await connection.open();
+    console.log('Connected!');
+  } catch (err) {
+    console.log('Failed to connect!');
+    process.exit(1);
+  }
+
+  connection.serverPackets.on(ServerPacketType.UNSUPPORTED, (type) => {
+    console.log('<< Received unsupported packet', { type });
+  });
+
+  connection.serverPackets.on(ServerPacketType.PONG, (echo) => {
+    console.log('<< Received pong', { echo });
+  });
+
+  connection.serverPackets.on(ServerPacketType.CHAT, (channel, message) => {
+    console.log('<< Received chat', { channel, message });
+  });
+
+  setTimeout(() => {
+    connection.clientPackets.emit(ClientPacketType.PING, 123);
+    console.log('>> Sent ping');
+  }, 2000);
+
+  setInterval(() => {
+    connection.clientPackets.emit(ClientPacketType.HEARTBEAT);
+    console.log('>> Sent heartbeat');
+  }, 25000);
+})();
---
+++ examples/login.ts
@@ -0,0 +1,4 @@
+import { Connection, ClientPacketType, ServerPacketType } from '../lib';
+
+const username = process.env.EL_USERNAME;
+const password = process.env.EL_PASSWORD;
