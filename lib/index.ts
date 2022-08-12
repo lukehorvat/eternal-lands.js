21b9e383e87a0a7e9df2aa3ec387b2e664d17ebf
Luke Horvat <lukehorvat@gmail.com>
2022-08-12 19:50:37 +0200

    Expose packet data types

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,12 @@
 export { Client } from './client';
-export { ClientPacket, ClientPacketType } from './packets/client';
-export { ServerPacket, ServerPacketType } from './packets/server';
+export {
+  ClientPacket,
+  ClientPacketData,
+  ClientPacketType,
+} from './packets/client';
+export {
+  ServerPacket,
+  ServerPacketData,
+  ServerPacketType,
+} from './packets/server';
 export * as Constants from './constants';
