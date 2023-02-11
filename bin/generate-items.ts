4143f11f49fe0416d2fc150ec752e29415e5356d
Luke Horvat <lukehorvat@gmail.com>
2023-02-11 02:03:01 +0100

    Generate additional item ID maps

--- bin/generate-items.ts
+++ bin/generate-items.ts
@@ -19,36 +19,48 @@ const items = fs
   });
 
 const itemNameOccurrences = new Map();
-const itemIdStr = `export enum ItemId {
-${items
-  .map((item) => {
-    // A few items have duplicate names. Track the occurrences so we can
-    // append a number to their enum key if necessary.
-    const occurrence = (itemNameOccurrences.get(item.name) ?? 0) + 1;
-    itemNameOccurrences.set(item.name, occurrence);
+const enumKeys = items.map((item) => {
+  // A few items have duplicate names. Track the occurrences so we can
+  // append a number to their enum key if necessary.
+  const occurrence = (itemNameOccurrences.get(item.name) ?? 0) + 1;
+  itemNameOccurrences.set(item.name, occurrence);
 
-    const enumKey = `${item.name}${occurrence > 1 ? ` ${occurrence}` : ''}`
-      .replace(/[\/-]/g, ' ')
-      .replace(/['"\.]/g, '')
-      .replace('&', 'and')
-      .replace('%', ' percent')
-      .replace(/\s+/g, '_')
-      .replace(/^\d+/, (num) => `_${num}`)
-      .toUpperCase();
-    const enumValue = item.id;
+  return `${item.name}${occurrence > 1 ? ` ${occurrence}` : ''}`
+    .replace(/[\/-]/g, ' ')
+    .replace(/['"\.]/g, '')
+    .replace('&', 'and')
+    .replace('%', ' percent')
+    .replace(/\s+/g, '_')
+    .replace(/^\d+/, (num) => `_${num}`)
+    .toUpperCase();
+});
 
-    return `  ${enumKey} = ${enumValue},`;
-  })
-  .join('\n')}
-}`;
-
-const fileStr = `
-// This file has been automatically generated using information parsed from the
-// latest version of the EL client's \`item_info.txt\`. Do not edit it manually.
-
-${itemIdStr}
-`;
+const generatedOutput =
+  `// This file has been automatically generated using information parsed from the\n` +
+  `// latest version of the EL client's \`item_info.txt\`. Do not edit it manually.\n` +
+  '\n' +
+  'export enum ItemId {\n' +
+  items.map((item, index) => `  ${enumKeys[index]} = ${item.id},`).join('\n') +
+  '\n}\n' +
+  '\n' +
+  'export const ItemImageIds: Record<ItemId, number> = {\n' +
+  items
+    .map((item, index) => `  [ItemId.${enumKeys[index]}]: ${item.imageId},`)
+    .join('\n') +
+  '\n};\n' +
+  '\n' +
+  'export const ItemEMUs: Record<ItemId, number> = {\n' +
+  items
+    .map((item, index) => `  [ItemId.${enumKeys[index]}]: ${item.emu},`)
+    .join('\n') +
+  '\n};\n' +
+  '\n' +
+  'export const ItemNames: Record<ItemId, string> = {\n' +
+  items
+    .map((item, index) => `  [ItemId.${enumKeys[index]}]: \`${item.name}\`,`)
+    .join('\n') +
+  '\n};\n';
 
 const outputPath = path.resolve(__dirname, '../lib/generated/items.ts');
 fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-fs.writeFileSync(outputPath, fileStr, 'utf8');
+fs.writeFileSync(outputPath, generatedOutput, 'utf8');
